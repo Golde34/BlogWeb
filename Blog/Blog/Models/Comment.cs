47a7aa3781c48d9c232abc73776ed733bad3ff00
Golde34 <82593925+Golde34@users.noreply.github.com>
2022-11-12 14:50:44 +0700

    add comment models
    add comment models

---
+++ Blog/Blog/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Models.Enums;
+
+namespace Blog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public int BlogID { get; set; }
+        public virtual Blogs Blogs { get; set; }
+        public string UserID { get; set; }
+        public virtual User User { get; set; }
+    }
+}
